1b2026c7012068ffd1911965e7542b04cddd94c0
Charles <charles.jouenne@gmail.com>
2024-05-13 13:33:21 +0200

    Correction d'une erreur dans la barre de recherche

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -162,42 +162,68 @@ function optionListener () {
 
 // Ecoute les tags sélectionnés
 function deletListener() {
+
     // Pour déselectionner un tag
     const deletIngredient = [...document.querySelectorAll(".element_ingredient")]
     const deletAppliance = [...document.querySelectorAll(".element_appliance")]
     const deletUstensils = [...document.querySelectorAll(".element_ustensils")]
 
-    // Si les aucun tags n'est sélectionné, appliquer le résultat de la barre de recherche générale
-    if (deletIngredient.length === 0 && deletAppliance.length === 0 && deletUstensils.length === 0) {
-        let generalSearch = document.querySelector("#search").value
-        searchRecipes(generalSearch)
-    } 
-    
-    else {
-        deletIngredient.forEach(p => {
-            p.addEventListener('click', () =>  {
-                deletItem(tagsToUpdate, p.textContent, "ingredients");
-                focusConstruct(tagsToUpdate, "ingredients")
+    deletIngredient.forEach(p => {
+        p.addEventListener('click', () =>  {
+            deletItem(tagsToUpdate, p.textContent, "ingredients");
+            focusConstruct(tagsToUpdate, "ingredients")
+
+            // Si les aucun tags n'est sélectionné, appliquer le résultat de la barre de recherche générale
+            if (tagsToUpdate.ingredients.length === 0 && tagsToUpdate.ustensils.length === 0 && tagsToUpdate.ustensils.length === 0) {
+                let generalSearch = document.querySelector("#search").value;
+                searchRecipes(generalSearch);
+                return;
+            } 
+                
+            // Sinon, met à jour l'affichage en tenant compte des tags sélectionnés
+            else {
                 newDisplay()
-            } )
-        })
+            }                
+        } )
+    })
         
-        deletAppliance.forEach(p => {
-            p.addEventListener('click', () => {
-                deletItem(tagsToUpdate, p.innerHTML, "appliance");
-                focusConstruct(tagsToUpdate, "appliance")
+    deletAppliance.forEach(p => {
+        p.addEventListener('click', () => {
+            deletItem(tagsToUpdate, p.innerHTML, "appliance");
+            focusConstruct(tagsToUpdate, "appliance")
+
+              // Si les aucun tags n'est sélectionné, appliquer le résultat de la barre de recherche générale
+              if (tagsToUpdate.ingredients.length === 0 && tagsToUpdate.ustensils.length === 0 && tagsToUpdate.ustensils.length === 0) {
+                let generalSearch = document.querySelector("#search").value;
+                searchRecipes(generalSearch);
+                return;
+            } 
+                
+            // Sinon, met à jour l'affichage en tenant compte des tags sélectionnés
+            else {
                 newDisplay()
-            })
+            }    
         })
+    })
     
-        deletUstensils.forEach(p => {
-            p.addEventListener('click', () => {
-                deletItem(tagsToUpdate, p.innerHTML, "ustensils");
-                focusConstruct(tagsToUpdate, "ustensils")
+    deletUstensils.forEach(p => {
+        p.addEventListener('click', () => {
+            deletItem(tagsToUpdate, p.innerHTML, "ustensils");
+            focusConstruct(tagsToUpdate, "ustensils")
+
+              // Si les aucun tags n'est sélectionné, appliquer le résultat de la barre de recherche générale
+            if (tagsToUpdate.ingredients.length === 0 && tagsToUpdate.ustensils.length === 0 && tagsToUpdate.ustensils.length === 0) {
+                let generalSearch = document.querySelector("#search").value;
+                searchRecipes(generalSearch);
+                return;
+            } 
+                
+            // Sinon, met à jour l'affichage en tenant compte des tags sélectionnés
+            else {
                 newDisplay()
-            } )
-        })
-    }  
+            }    
+        } )
+    })  
 }
 
 // Permet de vider le contenu de la zone de recherche
