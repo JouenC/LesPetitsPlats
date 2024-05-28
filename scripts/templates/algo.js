23577873b8615cfacbf9bb06b20ab1e94038461f
Charles <charles.jouenne@gmail.com>
2024-05-28 06:35:00 +0200

    Mise en place d'updateTagsDisplay

--- scripts/templates/algo.js
+++ scripts/templates/algo.js
@@ -284,8 +284,35 @@ function searchDropdown() {
         default:
             break;
     }
+    updateTagsDisplay();
     focusConstruct(tagsToUpdate, filterType);
     newDisplay()
+}
+
+function updateTagsDisplay() {
+    // Vider la liste des tags avant de la remplir
+    tagsList.innerHTML = "";
+
+    // Parcourir les catégories de tags
+    for (let type in tagsToUpdate) {
+        // Parcourir chaque tag dans la catégorie
+        tagsToUpdate[type].forEach(tag => {
+            // Créer une div pour le tag
+            let tagDiv = document.createElement("div");
+            tagDiv.className = `tag_item tag_${type}`;
+            tagDiv.textContent = tag;
+            console.log(tagDiv)
+
+            // Ajouter un bouton de suppression à chaque tag
+            let deleteButton = document.createElement("i");
+            deleteButton.className = "fa-solid fa-x delet_tag";
+            deleteButton.setAttribute("aria-hidden", "true");
+
+            tagDiv.appendChild(deleteButton);
+            tagsList.appendChild(tagDiv);
+            deletListener()
+        })   
+    }
 }
 
 // Met l'affichage à jour en fonction des tags
